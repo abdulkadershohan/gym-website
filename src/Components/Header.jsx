e9272569747018f6f50b97612ecee3e1e3c5f3ea
Abdul Kader <abdulkadirshohan@gmail.com>
2021-09-14 11:37:25 +0600

    OFFER ADDED

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,20 +1,18 @@
 import React from 'react';
 
 function Header(){
-        return (
-            <div id='main'>
-                <div className="header-heading">
-                    <h2 style={{fontSize:'40px'}}>STEP UP YOUR</h2>
-                    <h1 style={{fontSize:'5rem',marginTop:'-30px'}}><span><font color="red">FITNESS</font> </span>WITH US</h1>
-                    <p className="details">Build Your Body And Fitness With Professional Touch</p>
-                    <div className="header-btns">
-                        <a className='header-btn' style={{marginLeft:'30px',marginTop:'40px'}} href="#">JOIN US</a>
-                    </div>
+    return (
+        <div id='main'>
+            <div className="header-heading">
+                <h2>STEP UP YOUR</h2>
+                <h1><span>FITNESS</span>WITH US</h1>
+                <p className="details">Build Your Body And Fitness With Professional Touch</p>
+                <div className="header-btns">
+                    <a className='header-btn' href="#">JOIN US</a>
                 </div>
-
-
             </div>
-        );
+        </div>
+    );
 
 }
 
